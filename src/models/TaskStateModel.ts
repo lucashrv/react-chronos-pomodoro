15e2108bac161ef33f47b5568a08379756a13031
lucashrv <lucas.hrv@outlook.com>
2025-05-27 08:32:25 -0300

    feat: Task Models

---
+++ src/models/TaskStateModel.ts
@@ -0,0 +1,14 @@
+import type { TaskModel } from "./taskModel";
+
+export interface TaskStateModel {
+    task: TaskModel[];
+    secondsRemaining: number;
+    formattedSecondsRemaining: string;
+    activeTask: TaskModel | null;
+    currentCycle: number;
+    config: {
+        workTime: number;
+        shortBreakTime: number;
+        longBreakTime: number;
+    };
+}
